95d8715fde429d494c89cd7f372b47a4ea328041
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-11 20:41:00 +0100

    step-4: add routes for static views

--- app/js/app.js
+++ app/js/app.js
@@ -5,5 +5,14 @@ angular.module('foodme', ['ngRoute', 'customer'])
     .when('/', {
       controller: 'CustomerController',
       templateUrl: 'views/customerInfo.html'
+    })
+    .when('/who-we-are', {
+      templateUrl: 'views/who-we-are.html'
+    })
+    .when('/how-it-works', {
+      templateUrl: 'views/how-it-works.html'
+    })
+    .when('/help', {
+      templateUrl: 'views/help.html'
     });
 }]);
