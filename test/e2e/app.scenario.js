edcf0ff2c7a6816c2f46217a1bad34b7f6c45ff9
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-11 20:41:00 +0100

    step-2: Add end to end tests to check the validation is working

--- test/e2e/app.scenario.js
+++ test/e2e/app.scenario.js
@@ -1,3 +1,24 @@
 describe('foodme app', function() {
 
+  describe('Customer info form', function() {
+
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+
+
+    it('should add $invalid CSS class to invalid inputs', function() {
+      var nameInput = element(by.model('customer.name'));
+      nameInput.clear();
+      expect(nameInput.getAttribute('class')).toContain('ng-invalid');
+    });
+
+    it('should disable the button when a field is invalid', function() {
+      var nameInput = element(by.model('customer.name'));
+      var getRestaurantsButton = element(by.css('form[name="customerForm"] button'));
+      nameInput.clear();
+      expect(getRestaurantsButton.getAttribute('disabled')).toEqual('true');
+    });
+  });
+
 });
