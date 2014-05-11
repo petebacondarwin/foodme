3a7f346dd039e2bf48525c25fc802c415d31a464
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-11 20:41:00 +0100

    step-6: add static restaurant data to RestaurantController

--- app/js/restaurants.js
+++ app/js/restaurants.js
@@ -6,4 +6,28 @@ angular.module('restaurants', ['customer'])
     $location.path('/customer-info');
   }
 
+  $scope.restaurants = [
+    {
+      "id":"angular",
+      "name":"Angular Pizza",
+      "cuisine":"pizza",
+      "price":1,
+      "rating":5,
+      "description":"Home of the superheroic pizza!"
+    },{
+      "id":"tofuparadise",
+      "name":"BBQ Tofu Paradise",
+      "cuisine":"vegetarian",
+      "price":2,
+      "rating":1,
+      "description":"Vegetarians, we have your BBQ needs covered. Our home-made tofu skewers and secret BBQ sauce will have you licking your fingers."
+    },{
+      "id":"beijing",
+      "name":"Beijing Express",
+      "cuisine":"chinese",
+      "price":2,
+      "rating":4,
+      "description":"Fast, healthy, Chinese food. Family specials for takeout or delivery. Try our Peking Duck!"
+    }
+  ];
 }]);
